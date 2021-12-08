29f4b6bbcbf68a7d09c3969be79e7d7fb7c16db3
coelhovinicius <coelhovinicius@yahoo.com.br>
2021-12-08 17:42:39 -0300

    Excecoes Personalizadas - Parte 2

---
+++ Aula144_ExcecoesPersonalizadasParte2/Entities/Booking.cs
@@ -0,0 +1,54 @@
+﻿/* >>> CLASSE BOOKING <<< */
+using System;
+
+namespace Aula144_ExcecoesPersonalizadasParte2.Entities
+{
+    class Booking
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Booking()
+        {
+        }
+
+        public Booking(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays; // Casting do double TotalDays para inteiro
+        }
+
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Rebooking error: Dates must be later than today";
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Booking error: Check-out date must be later than check-in date";
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+            return null; // Indica que nao houve nenhum erro
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber
+                + ", check-in: " + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: " + CheckOut.ToString("dd/MM/yyyy")
+                + ", " + Duration() + " nights";
+        }
+    }
+}
---
+++ Aula144_ExcecoesPersonalizadasParte2/Program.cs
@@ -0,0 +1,57 @@
+﻿/* SOLUCAO 2 - RUIM - RETORNANDO UMA STRING INFORMANDO O ERRO 
+ 
+    • A semântica da operação é prejudicada
+    • Retornar string não tem nada a ver com atualização de reserva
+    • E se a operação tivesse que retornar um string?
+    • Ainda não é possível tratar exceções em construtores
+    • A lógica fica estruturada em condicionais aninhadas
+*/
+
+/* >>> PROGRAMA PRINCIPAL <<< */
+using System;
+using Aula144_ExcecoesPersonalizadasParte2.Entities;
+
+namespace Aula144_ExcecoesPersonalizadasParte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room Number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Booking error: Check-in date must be earlier than check-out date");
+            }
+            else
+            {
+                Booking booking = new Booking(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Booking: " + booking);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter new dates for rebooking: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = booking.UpdateDates(checkIn, checkOut);
+
+                if (error != null)
+                {
+                    Console.WriteLine("Booking error: " + error);
+                }
+
+                else
+                {
+                    Console.WriteLine("Booking: " + booking);
+                }
+            }
+        }
+    }
+}
